2f14800d156f8cac68fc6027d7d93b7211dcb1c3
phuc.dinh <phucdt4697@gmail.com>
2023-05-25 21:54:37 +0700

    (refac) test embed

--- public/embed.js
+++ public/embed.js
@@ -6,7 +6,7 @@ style.innerHTML = `
     .w-100 { width: 100%; height: 100%; } .w-fill { width: 100px; height: 100px; } .w-uw { width: 600px; height: 100%; }
     #hook-iframe { position: absolute !important; bottom: 0px !important; right: 0px !important; border: none !important; z-index: 100000 !important; }
 `;
-document.head.insertBefore(style);
+document.head.insertBefore(style, document.head.firstElementChild);
 console.log(document.head);
 
 window.addEventListener('load', () => {
